9bc59505c6eee47f75a2e4e66614cb1957786e38
Bryan Cha <bryancha@Bryans-MacBook-Air.local>
2025-05-25 15:34:54 -0700

    debug add{

--- core/admin.py
+++ core/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
 from .models import React
+import logging
 
-# Create a resource class for React model
+logger = logging.getLogger(__name__)
+logger.info("Registering React admin...")
+
+# Resource for import/export
 class ReactResource(resources.ModelResource):
     class Meta:
         model = React
-        fields = ("id", "name", "detail")  # Match CSV headers and model fields exactly
+        fields = ("id", "name", "detail")  # Must match your model
 
+# Register model in admin with import/export enabled
 @admin.register(React)
 class ReactAdmin(ImportExportModelAdmin):
     resource_class = ReactResource
--- quotes/core/admin.py
+++ quotes/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+from .models import React
+import logging
 
-# Register your models here.
+logger = logging.getLogger(__name__)
+logger.info("Registering React admin...")
+
+# Resource for import/export
+class ReactResource(resources.ModelResource):
+    class Meta:
+        model = React
+        fields = ("id", "name", "detail")  # Must match your model
+
+# Register model in admin with import/export enabled
+@admin.register(React)
+class ReactAdmin(ImportExportModelAdmin):
+    resource_class = ReactResource
