393e4abe7e63d88dcbbf288c594bbccc4dcc610d
Bryan Cha <bryancha@Bryans-MacBook-Air.local>
2025-05-25 14:50:47 -0700

    debug admin

--- core/admin.py
+++ core/admin.py
@@ -3,14 +3,12 @@ from import_export.admin import ImportExportModelAdmin
 from .models import React
 from django.contrib.auth import get_user_model
 
-admin.site.register(React)
-
-User = get_user_model()
-# Register your models here.
-
-if not User.objects.filter(username="admin123").exists():
-    User.objects.create_superuser("admin123", "admin@example.com", "tempPassword123")
-
+# Register React model once
 @admin.register(React)
 class ReactAdmin(ImportExportModelAdmin):
     pass
+
+# Create superuser at deploy if not exists
+User = get_user_model()
+if not User.objects.filter(username="admin123").exists():
+    User.objects.create_superuser("admin123", "admin@example.com", "tempPassword123")
