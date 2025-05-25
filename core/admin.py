e2f3d23493abb62a3f4235e72add65cd5d50d026
Bryan Cha <bryancha@Bryans-MacBook-Air.local>
2025-05-25 14:41:18 -0700

    Superuser Password

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import React
+from django.contrib.auth import get_user_model
 
+admin.site.register(React)
+
+User = get_user_model()
 # Register your models here.
+
+if not User.objects.filter(username="admin123").exists():
+    User.objects.create_superuser("admin123", "admin@example.com", "tempPassword123")
